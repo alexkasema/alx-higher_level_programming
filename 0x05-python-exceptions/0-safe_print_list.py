2b8ee6380a4696e56c41bf90e2c8848f17789a9f
alexkasema <lexxmulwa@gmail.com>
2023-11-20 02:01:11 -0800

    A function that prints x elements of a list. x can be bigger than the length of my_list Returns the real number of elements printed.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin//python3
-
 def safe_print_list(my_list=[], x=0):
 
     count = 0
@@ -8,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             count += 1
-        except IndexError:
+        except:
             continue
     print()
     return(count)
