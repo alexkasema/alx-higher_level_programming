925d058f4ced339c96de32cee1894de33ba69dbe
alexkasema <lexxmulwa@gmail.com>
2023-11-21 05:02:55 -0800

    A class Square that defines a square by a size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Square with size"""
+
+
+class Square:
+    """ A class Square that defines a square by its size """
+
+    def __init__(self, size):
+        """ Initializing a new square.
+
+        Args:
+            size (int): Length of the created square.
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,5 @@
 ## Python - Classes and Objects
 # 0-square.py
 Write an empty class Square that defines a square
+# 1-square.py
+A class Square that defines a square by its size. Private instance attribute: size
