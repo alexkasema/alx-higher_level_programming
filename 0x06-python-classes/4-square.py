5412c03fee0f755ae93b9a10c52fcaeacdc1c4c2
alexkasema <lexxmulwa@gmail.com>
2023-11-21 06:08:19 -0800

    A class that creates a square sets its property getter and setter and cal its area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" Access and update private attribute """
+
+class Square:
+    """ A class that sets the properties getter and setter and
+        calculates the area of the square
+    """
+
+    def __init__(self, size=0):
+        """ Initialize an instance of a square.
+        Args:
+            size (int): The size of a square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Return size of the square """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of a square """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ calculates the area of a square
+        Return:
+            The area of the square
+        """
+        return (self.__size ** 2)
+
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -7,3 +7,5 @@ A class Square that defines a square by its size. Private instance attribute: si
 A class Square that defines a square by a size. size must be an integer, otherwise raise a TypeError exception with the message size must be an integer. if size is less than 0, raise a ValueError exception with the message size must be >= 0.
 # 3-square.py
 A class Square that defines a square by size and calculates its area. 
+# 4-square.py
+A class that defines a square sets its properties getter and setter and calculates the area of the square
