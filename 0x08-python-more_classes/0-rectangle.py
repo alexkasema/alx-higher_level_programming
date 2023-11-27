d88fb8858928e579e8a30c493bad43731fe49f0c
alexkasema <lexxmulwa@gmail.com>
2023-11-27 07:09:40 -0800

    An empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+""" Simple rectangle """
+
+class Rectangle:
+    """ A class that defines a rectangle """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+## Python - More Classes and Objects
+# 0-rectangle.py
+An empty class Rectangle that defines a rectangle.
