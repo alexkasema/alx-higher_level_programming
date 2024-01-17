c15f8e5c393aff226b03f395b4b29c7556a27b36
alexkasema <lexxmulwa@gmail.com>
2024-01-17 08:40:04 -0800

    A script that lists all cities contained in the database..

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- A script that lists all cities contained in the database.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
---You can use only one SELECT statement
+-- You can use only one SELECT statement
 
 SELECT cities.id, cities.name, states.name
 FROM cities
