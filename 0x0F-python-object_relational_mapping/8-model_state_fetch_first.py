d1b809fab14eedebd8da23bca741523e86e124b5
alexkasema <lexxmulwa@gmail.com>
2024-02-16 04:19:00 -0800

    A script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -28,4 +28,7 @@ if __name__ == '__main__':
 
     state = session.query(State).order_by(State.id).first()
 
-    print("{}: {}".format(state.id, state.name))
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("No states")
